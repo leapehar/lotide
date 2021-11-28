1ff5d20a1a0b06fc830089faf4c73dbb0224414f
Lea Pehar <leapehar@gmail.com>
2021-11-28 04:03:57 +0000

    adding findKey.js

---
+++ findKey.js
@@ -0,0 +1,25 @@
+//Challenge
+
+//Implement the function findKey which takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined.
+
+const findKey = function (object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+
+
+let result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2);
+
+console.log(result1);
