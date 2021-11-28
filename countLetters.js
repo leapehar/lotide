1eb83875ff6d8ae57602924ffd38cfba58380e69
Lea Pehar <leapehar@gmail.com>
2021-11-28 04:00:12 +0000

    adding countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed:  ${actual} === ${expected}`);
+  }
+};
+
+
+const countLetters = function (string) {
+  const results = {};
+  for (const letter of string) {
+
+    // console.log(letter);
+    // console.log(results);
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+  console.log(results);
+}
+
+countLetters('hellobgrvfevfev dewcew cewcew ');
+
