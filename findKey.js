97e32d3eb61eedfb4809915f95d1ba8b2daf2204
Lea Pehar <leapehar@gmail.com>
2021-12-02 06:41:10 +0000

    adding changes to findKey.js

--- findKey.js
+++ findKey.js
@@ -2,7 +2,7 @@
 
 //Implement the function findKey which takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined.
 
-const findKey = function (object, callback) {
+const findKey = function(object, callback) {
   for (let key in object) {
     if (callback(object[key])) {
       return key;
