87eb12ed5f9cd52ca997cd963080f41bf823553b
Lea Pehar <leapehar@gmail.com>
2021-12-02 22:29:42 +0000

    refactored head.js

--- head.js
+++ head.js
@@ -1,24 +1,12 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed:  ${actual} === ${expected}`);
-  }
-};
-
-// TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
 
 
 let array = [];
 
-const head = function (arr) {
+const head = function(arr) {
   let firstElement = arr[0];
   return firstElement;
 }
 
 console.log(head(array));
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
