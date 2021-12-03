21f423fd63b00f6e511fd58ddd12babeab94b9a0
Lea Pehar <leapehar@gmail.com>
2021-12-03 05:55:25 +0000

    adding edits to eqArraysTest.js

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,12 +1,39 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+// const eqArrays = require('../eqArrays');
+
+
+
+// // eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+// // eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+// // eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false); // => should PASS
+
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
+describe("#eqArrays", () => {
+  it("returns true for [1, 2, 3], [3, 2, 1]", () => {
+    assert.notEqual(eqArrays([1, 2, 3], [3, 2, 1]));
+  });
+
+  it(`returns true for ["1", "2", "3"], ["1", "2", "3"]`, () => {
+    assert.equal(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+  });
+
+  it(`returns true for ["1", "2", "3"], ["1", "2", 3]`, () => {
+    assert.notEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+  });
+
+  it(`returns true for [1, 2, 3], [1, 2, 3]), true`, () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+
+  it(`returns true for [1, 2, 3], [1, 2, 4]), true`, () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
+  });
 
 
-// eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false); // => should PASS
+});
