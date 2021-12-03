e3c5f1728075f3025d49e25476b0834750099f9e
Lea Pehar <leapehar@gmail.com>
2021-12-03 22:22:20 +0000

    cleaned up file, removed not necessary comments

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,7 @@
 const eqArrays = require("./eqArrays");
 
-
-
-
 const assertArraysEqual = function(arr1, arr2) {
 
-
-  //const result = eqArrays(arr1, arr2);
-  //console.log(result);
 
   if (eqArrays(arr1, arr2)) {
     console.log(`Assertion Passed ${arr1} === ${arr2}`);
