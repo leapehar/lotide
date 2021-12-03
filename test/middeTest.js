a2ff0825529e897a6681a4ad867f8c514f5e6a09
Lea Pehar <leapehar@gmail.com>
2021-12-03 22:46:18 +0000

    cleaned up file, removed not necessary comments

--- test/middeTest.js
+++ test/middeTest.js
@@ -1,13 +1,5 @@
 const assert = require('chai').assert;
-
-
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
-//const eqArrays = require('../eqArrays');
-
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); //passes
-// assertArraysEqual(middle([1, 2, 3, 5, 6]), [3, 4]); //fails
-
 
 describe("#middle", () => {
   it("returns true for [1, 2, 3, 4, 5, 6]), [3, 4]", () => {
@@ -17,8 +9,4 @@ describe("#middle", () => {
   it("returns false for [1, 2, 3, 5, 6]), [3, 4]", () => {
     assert.notDeepEqual(middle([1, 2, 3, 5, 6]), [3, 4]);
   });
-
-
-
-
 });
