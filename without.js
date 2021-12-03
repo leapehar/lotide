09390908c2bb6fcf7bdaf1af1c5760dcc3ca7fc2
Lea Pehar <leapehar@gmail.com>
2021-12-03 22:38:41 +0000

    cleaned up file, removed not necessary comments

--- without.js
+++ without.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -6,24 +6,19 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-
-const eqArrays = function (arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
-
   for (let i = 0; i < arr1.length; i++) {
-
     if (arr1[i] !== arr2[i]) {
       return false;
     }
-
   }
   return true;
 }
 
-const assertArraysEqual = function (arr1, arr2) {
-
+const assertArraysEqual = function(arr1, arr2) {
 
   const result = eqArrays(arr1, arr2);
   console.log(result);
@@ -35,11 +30,7 @@ const assertArraysEqual = function (arr1, arr2) {
   console.log('Assertion Failed');
 }
 
-//assertArraysEqual([1, 2, 3], [1, 2, 3]);
-
-
-
-const without = function (source, itemsToRemove) {
+const without = function(source, itemsToRemove) {
   let newArray = [];
   for (let i = 0; i < source.length; i++) {
     if (!itemsToRemove.includes(source[i])) {
@@ -49,14 +40,8 @@ const without = function (source, itemsToRemove) {
   return newArray;
 }
 
-
-
-
 assertEqual(eqArrays(without([1, 2, 3], [1]), [2, 3]), true);
 
-//This function should take in a source array and a itemsToRemove array. It should return a new array with only those elements from source that are not present in the itemsToRemove array.
-
-//Use assertArraysEqual to write test cases for various scenarios.
 console.log(without([1, 2, 3], [1]))
 without([1, 2, 3], [1]) // => [2, 3]
 without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
