a7d26091d0a914ffecc83124e649588d01f29448
Lea Pehar <leapehar@gmail.com>
2021-12-03 22:26:00 +0000

    cleaned up file, removed not necessary comments

--- countLetters.js
+++ countLetters.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -6,8 +6,7 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-
-const countLetters = function (string) {
+const countLetters = function(string) {
   const results = {};
   for (const letter of string) {
 
