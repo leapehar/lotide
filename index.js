1823b522441b3c1bddfbb695fb9d6867666902da
Lea Pehar <leapehar@gmail.com>
2021-12-03 18:20:58 +0000

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
-const assertArraysEqual = require('.assertArraysEqual');
+const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
 const assertObjectsEqual = require('./assertObjectsEqual');
 const countLetters = require('./countLetters');
@@ -10,31 +10,46 @@ const eqArrays = require('./eqArrays');
 const eqObjects = require('./eqObjects');
 const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
-const letterPositions = require('./letterPositions');
+//const letterPositions = require('./letterPositions');
 const map = require('./map');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
 
 
-
-
-
+// module.exports = {
+//   head: head,
+//   tail: tail,
+//   middle: middle,
+//   assertArraysEqual: assertArraysEqual,
+//   assertEqual: assertEqual,
+//   assertObjectsEqual: assertObjectsEqual,
+//   countLetters: countLetters,
+//   countOnly: countOnly,
+//   eqArrays: eqArrays,
+//   eqObjects: eqObjects,
+//   findKey: findKey,
+//   findKeyByValue: findKeyByValue,
+//   letterPositions: letterPositions,
+//   map: map,
+//   takeUntil: takeUntil,
+//   without: without
+// };
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  letterPositions: letterPositions,
-  map: map,
-  takeUntil: takeUntil,
-  without: without
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  //letterPositions,
+  map,
+  takeUntil,
+  without
 };
