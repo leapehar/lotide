8454fc4bd46120cf106286f1ebe34637c925a708
Lea Pehar <leapehar@gmail.com>
2021-12-03 22:35:13 +0000

    cleaned up file, removed not necessary comments

--- middle.js
+++ middle.js
@@ -1,40 +1,7 @@
-
-// const eqArrays = function(arr1, arr2) {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
-
-//   for (let i = 0; i < arr1.length; i++) {
-
-//     if (arr1[i] !== arr2[i]) {
-//       return false;
-//     }
-
-//   }
-//   return true;
-// }
-
-// const assertArraysEqual = function(arr1, arr2) {
-
-
-//   const result = eqArrays(arr1, arr2);
-//   //console.log(result);
-
-//   if (result) {
-//     console.log('Assertion Passed');
-//     return;
-//   }
-//   console.log('Assertion Failed');
-// }
-
-//assertArraysEqual([1, 2, 3], [1, 2, 3]);
-
 const middle = function(array) {
-  //let newArray = [];
+
   let middle = Math.floor(array.length / 2);
   let result;
-  //console.log(middle);
-  //console.log(array[middle]);
   if (array.length <= 2) {
     result = [];
     return result;
@@ -45,14 +12,7 @@ const middle = function(array) {
     result = [array[middle - 1], array[middle]];
     return result;
   }
-
 }
-//console.log(middle([1, 2, 3, 4, 5, 6]));
-//console.log(middle([1, 2, 7, 4, 5]));
-
-//use the assertArraysEqual function for testing our new function.
-
-
 
 
 module.exports = middle;
