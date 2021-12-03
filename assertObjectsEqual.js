a9de69d81cb6bb1d7c0b4f51ec656c89e8949359
Lea Pehar <leapehar@gmail.com>
2021-12-03 22:25:11 +0000

    cleaned up file, removed not necessary comments

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,4 +1,4 @@
-const eqObjects = function (object1, object2) {
+const eqObjects = function(object1, object2) {
 
   //arrays of keys for object1 and object2
   let keyObj1 = Object.keys(object1);
@@ -34,23 +34,9 @@ const abc = { a: "1", b: "2", c: "3" };
 
 
 // FUNCTION IMPLEMENTATION
-// const assertObjectsEqual = function (actual, expected) {
-//   const inspect = require('util').inspect;
-
-//   // Implement me!
-
-//   if (actual === expected) {
-//     console.log(`Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
-//     return;
-//   }
-//   console.log(`Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
-// };
-
-// console.log(assertObjectsEqual(eqObjects(ab, ba), false));
-// console.log(assertObjectsEqual(eqObjects(ab, abc), false));
 
 
-const assertObjectsEqual = function (actual, expected) {
+const assertObjectsEqual = function(actual, expected) {
 
   //displays the output of ${actual} and ${expected}
   const inspect = require('util').inspect;
