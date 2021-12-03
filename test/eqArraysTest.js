247711572c5ed9c3e1ce6664ef7847523249121e
Lea Pehar <leapehar@gmail.com>
2021-12-03 22:44:04 +0000

    cleaned up file, removed not necessary comments

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,20 +1,8 @@
-// const assertEqual = require('../assertEqual');
-// const eqArrays = require('../eqArrays');
-
-
-
-// // eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-// // eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-// // eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false); // => should PASS
-
 const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
 describe("#eqArrays", () => {
+
   it("returns true for [1, 2, 3], [3, 2, 1]", () => {
     assert.notEqual(eqArrays([1, 2, 3], [3, 2, 1]));
   });
@@ -34,6 +22,5 @@ describe("#eqArrays", () => {
   it(`returns true for [1, 2, 3], [1, 2, 4]), true`, () => {
     assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
   });
-
 
 });
