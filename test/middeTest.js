679814685548ba6e727a996b4de91aba39888688
Lea Pehar <leapehar@gmail.com>
2021-12-03 05:54:45 +0000

    adding edits to middleTest.js

--- test/middeTest.js
+++ test/middeTest.js
@@ -1,6 +1,24 @@
+const assert = require('chai').assert;
+
+
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 //const eqArrays = require('../eqArrays');
 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); //passes
-assertArraysEqual(middle([1, 2, 3, 5, 6]), [3, 4]); //fails
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); //passes
+// assertArraysEqual(middle([1, 2, 3, 5, 6]), [3, 4]); //fails
+
+
+describe("#middle", () => {
+  it("returns true for [1, 2, 3, 4, 5, 6]), [3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+  it("returns false for [1, 2, 3, 5, 6]), [3, 4]", () => {
+    assert.notDeepEqual(middle([1, 2, 3, 5, 6]), [3, 4]);
+  });
+
+
+
+
+});
